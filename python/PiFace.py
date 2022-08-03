b14efb7f55a8d20412943ef71ed896be84eba023
Rebix <timon@set-richter.de>
2022-08-03 23:18:35 +0200

    made it possible to read the dada in the python script

--- python/PiFace.py
+++ python/PiFace.py
@@ -1,2 +1,26 @@
-print("Test Completed")
-print("Test 2 Completed")
+import numpy as np
+import pandas as pd
+import pickle
+
+print("PiFace Started")
+
+
+def load_csv(filepath):
+    data = []
+    col = []
+    checkcol = False
+    with open(filepath) as f:
+        for val in f.readlines():
+            val = val.replace("\n", "")
+            val = val.split(',')
+            if checkcol is False:
+                col = val
+                checkcol = True
+            else:
+                data.append(val)
+    df = pd.DataFrame(data=data, columns=col)
+    return df
+
+
+filedata = load_csv("/home/pi/information.txt")
+print(filedata)
